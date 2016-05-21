50002fb943a34f04e5ee7f71faf159849a79a368
Irakli Murusidze <iraklimurusidze@live.com>
2016-05-21 19:43:58 +0400

    Added_Question_and_Answer_POCO_Classes_in_Domain

---
+++ BusinessCredit.TestEngine.Domain/Answer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCredit.TestEngine.Domain
+{
+    class Answer
+    {
+        public int AnswerID { get; set; }
+        public object Content { get; set; }
+        public AnswerContentType ContentType { get; set; }
+
+
+        public enum AnswerContentType
+        {
+            Text,
+            Picture,
+            Audio,
+            Video,
+            Html
+        }
+    }
+}
---
+++ BusinessCredit.TestEngine.Domain/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCredit.TestEngine.Domain
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public QuestionContentType ContentType { get; set; }
+        public string TextContent { get; set; }
+        public string ImageUrl { get; set; }
+        public string AudioUrl { get; set; }
+        public string VideoUrl { get; set; }
+        public string HtmlContent { get; set; }
+    }
+
+    public enum QuestionContentType
+    {
+        Text,
+        Picture,
+        Audio,
+        Video,
+        Html
+    }
+}
